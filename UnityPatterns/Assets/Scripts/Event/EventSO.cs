590bf1740e7b0ec8d813276e847367acde317ced
Dima <seaqqullprogramming@gmail.com>
2019-08-11 22:48:29 +0300

    Update: Event pattern

---
+++ UnityPatterns/Assets/Scripts/Event/EventSO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Event/Create")]
+public class EventSO : ScriptableObject
+{
+    private List<IEventListener> _eventListeners =
+        new List<IEventListener>();
+
+
+    public void Raise()
+    {
+        for (int i = _eventListeners.Count - 1; i >= 0; i--)
+            _eventListeners[i].OnEventRaised();
+    }
+
+    public void RegisterListener(IEventListener listener)
+    {
+        if (!_eventListeners.Contains(listener))
+            _eventListeners.Add(listener);
+    }
+
+    public void UnregisterListener(IEventListener listener)
+    {
+        if (_eventListeners.Contains(listener))
+            _eventListeners.Remove(listener);
+    }
+}
---
+++ UnityPatterns/Assets/Scripts/Event/IEventListener.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.Events;
+
+public interface IEventListener
+{
+    EventSO Event { get; }
+
+    UnityEvent Response { get; }
+
+    void OnEnable();
+
+    void OnDisable();
+
+    void OnEventRaised();    
+}
